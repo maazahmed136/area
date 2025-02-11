66b8c6ec765a2a4f707ec5ed7a367f5dc3ad4e8a
maazahmed136 <maazahmed0787@gmail.com>
2025-02-11 16:50:56 +0530

    fisrt

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+       'hero-image': "url('/images/header-background.jpg)",
+      },
+    },
+  },
+  plugins: [],
+}
+
